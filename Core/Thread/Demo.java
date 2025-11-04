dfc68f5f025b41c4b4fe64ad6e752749a667bf06
kushal bhargava <kushal.git01@gmail.com>
2025-11-04 15:56:48 +0530

    added Thread and Exception

---
+++ Core/Thread/Demo.java
@@ -0,0 +1,24 @@
+class A extends Thread{
+    public void run(){
+        for(int i=0; i<50; i++){
+            System.out.println("Hi ");
+        }
+    }
+}
+
+class B extends Thread{
+    public void run(){
+        for(int i=0; i<50; i++){
+            System.out.println("Hello ");
+        }
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        A obj1 = new A();
+        B obj2 = new B();
+        obj1.start();
+        obj2.start();
+    }    
+}
