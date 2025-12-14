69a705aa4f33e6511469dee56020bf107b87caf9
kushal bhargava <kushal.git01@gmail.com>
2025-12-14 21:12:49 +0530

    Collection and maven demo added

---
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.spring_sec_demo;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(HttpServletRequest request){
+        return "Hello World" + request.getSession().getId();
+    }
+
+}
---
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.spring_sec_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+
+        http
+        .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
