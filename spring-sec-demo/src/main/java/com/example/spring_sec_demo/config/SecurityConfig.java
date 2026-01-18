59f3d3f378d26fcc90c443293b427a9e14a966b4
kushal bhargava <kushal.git01@gmail.com>
2026-01-18 21:56:11 +0530

    jwt auth initial setup

--- spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
+++ spring-sec-demo/src/main/java/com/example/spring_sec_demo/config/SecurityConfig.java
@@ -1,27 +1,69 @@
 package com.example.spring_sec_demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http){
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-        http
-        .csrf(customizer -> customizer.disable())
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+
+    @Bean
+    public AuthenticationProvider authProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        return provider;
+    }
+
+
+
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/register").permitAll()
+                        .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
-                .sessionManagement(session ->
-                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
+
+
+
+
+
+    /*
+     * @Bean public UserDetailsService userDetailsService() {
+     *
+     * UserDetails user=User .withDefaultPasswordEncoder() .username("navin")
+     * .password("n@123") .roles("USER") .build();
+     *
+     * UserDetails admin=User .withDefaultPasswordEncoder() .username("admin")
+     * .password("admin@789") .roles("ADMIN") .build();
+     *
+     * return new InMemoryUserDetailsManager(user,admin); }
+     */
+
+
 }
